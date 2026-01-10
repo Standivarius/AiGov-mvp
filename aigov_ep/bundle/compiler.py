83581c6018211f2765397f132a3c5817c43d4a7c
Marius Moldovan <128666352+Standivarius@users.noreply.github.com>
2026-01-10 06:16:12 +0200

    Add aigov_ep internal module layout (skeleton placeholders) (#4)

---
+++ aigov_ep/bundle/compiler.py
@@ -0,0 +1,4 @@
+"""Skeleton placeholder for EP migration."""
+
+def compile_bundle():
+    raise NotImplementedError("compile_bundle: NOT IMPLEMENTED (skeleton)")
---
+++ aigov_ep/execute/runner.py
@@ -0,0 +1,4 @@
+"""Skeleton placeholder for EP migration."""
+
+def run_stage_a():
+    raise NotImplementedError("run_stage_a: NOT IMPLEMENTED (skeleton)")
